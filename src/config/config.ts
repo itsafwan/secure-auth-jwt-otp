6251ed4d929c53953db340713a87ff80bb374279
Muhammad Safwan <muhammadsafwan20051@gmail.com>
2026-05-29 18:02:27 +0500

    otp

--- src/config/config.ts
+++ src/config/config.ts
@@ -7,7 +7,11 @@ configDotenv()
 const requiredEnvVariables = [
   "MONGO_URI",
   "PORT",
-  "JWT_SECRET"
+  "JWT_SECRET",
+  "GOOGLE_CLIENT_ID",
+  "GOOGLE_CLIENT_SECRET",
+  "GOOGLE_REFRESH_TOKEN",
+  "GOOGLE_USER"
 ];
 
 
@@ -21,6 +25,12 @@ const config = {
   port: process.env.PORT || 3000,
   Mongo: process.env.MONGO_URI  || "" ,
   Jwt : process.env.JWT_SECRET || "",
+  Google: {
+    clientId: process.env.GOOGLE_CLIENT_ID || "",
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+    refreshToken: process.env.GOOGLE_REFRESH_TOKEN || "",
+    user: process.env.GOOGLE_USER || ""
+  }
 }
 
 
---
+++ src/services/email.services.ts
@@ -0,0 +1,23 @@
+import nodemailer from "nodemailer";
+import config from "../config/config.js";
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user:config.Google.user,
+    clientId: config.Google.clientId,
+    clientSecret: config.Google.clientSecret,
+    refreshToken: config.Google.refreshToken,
+  }
+})
+
+
+transporter.verify((error, success) => {
+  if (error) {
+    console.error("Email server connecting error:", error);
+  } else {
+    console.log("Email server is ready to send messages!");
+  }
+});
+
+export default transporter;
